6ed03c3f4be2366681daf5d8d050d4d404e85e9d
Che-tsun Ho <republicho@foxmail.com>
2023-01-31 20:55:40 +0800

    Update SimpleTodosManagerApplication.java

--- src/main/java/com/example/simpletodosmanager/SimpleTodosManagerApplication.java
+++ src/main/java/com/example/simpletodosmanager/SimpleTodosManagerApplication.java
@@ -1,12 +1,13 @@
 package com.example.simpletodosmanager;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
+
 import java.util.TimeZone;
 
 @SpringBootApplication
