93df0ecddf2b05ea96847be156654eb204f28f73
Che-tsun Ho <republicho@foxmail.com>
2023-02-02 11:08:21 +0800

    Update TODOItemController.java

--- src/main/java/com/example/simpletodosmanager/controller/TODOItemController.java
+++ src/main/java/com/example/simpletodosmanager/controller/TODOItemController.java
@@ -2,15 +2,17 @@ package com.example.simpletodosmanager.controller;
 
 import com.example.simpletodosmanager.exception.ResourceNotFoundException;
 import com.example.simpletodosmanager.model.TODOItem;
+import com.example.simpletodosmanager.model.User;
 import com.example.simpletodosmanager.service.TODOItemService;
 import com.example.simpletodosmanager.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.security.Principal;
 
-import java.util.List;
-import java.util.Optional;
 
 /**
  * @Author Anthony HE, anthony.zj.he@outlook.com
@@ -31,27 +33,65 @@ public class TODOItemController {
         this.userService = userService;
     }
 
-//    @PostMapping("/todos")
-//    public TODOItem createToDoItem(@Valid @RequestBody TODOItem item){
-//        return toDosRepository.save(item);
-//    }
+    @PostMapping("/todos")
+    public String createToDoItem(@Valid @RequestBody TODOItem item){
 
-//    @GetMapping("/todos")
-//    public List<TODOItem> getAllToDoItems(){
-//        // sort by the due date
-//        return toDosRepository.findAll(Sort.by(Sort.Direction.ASC, "dueDate"));
-//    }
-//
-//    @PostMapping("/todos/{id}")
-//    public TODOItem getToDoItemById(@PathVariable(value = "id") Long itemId){
-//        return toDosRepository.findById(itemId)
-//                .orElseThrow(() -> new ResourceNotFoundException("Item", "id", itemId));
-//    }
-//    @PostMapping("/todos/{name}")
-//    public Optional<TODOItem> getToDoItemByItemName(@PathVariable(value = "name") String name){
-//        return Optional.ofNullable(toDosRepository.findByItemName(name)
-//                .orElseThrow(() -> new ResourceNotFoundException("Item", "id", name)));
-//    }
+        todoItemService.createItem(item);
+        return "redirect:/todos";
+
+    }
+
+
+    @GetMapping("/todos")
+    public String getToDoItems(Model model, Principal principal, SecurityContextHolderAwareRequestWrapper request){
+
+        User signedUser = userService.getUserById(Long.parseLong(principal.getName()));
+        boolean isAdminSigned = request.isUserInRole("ROLE_ADMIN");
+        model.addAttribute("to-dos", todoItemService.findAll());
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("signedUser", signedUser);
+        model.addAttribute("isAdminSigned", isAdminSigned);
+        return "redirect:/todos";
+    }
+
+    @GetMapping("/todos/in-pogress")
+    public String getToDoItemsInProgress(Model model, Principal principal, SecurityContextHolderAwareRequestWrapper request){
+
+        User signedUser = userService.getUserById(Long.parseLong(principal.getName()));
+        boolean isAdminSigned = request.isUserInRole("ROLE_ADMIN");
+        model.addAttribute("onlyInProgress", true);
+        model.addAttribute("to-dos", todoItemService.findAll());
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("signedUser", signedUser);
+        model.addAttribute("isAdminSigned", isAdminSigned);
+        return "redirect:/todos";
+    }
+
+    @GetMapping("/todos/completed")
+    public String getToDoItemsCompleted(Model model, Principal principal, SecurityContextHolderAwareRequestWrapper request){
+
+        User signedUser = userService.getUserById(Long.parseLong(principal.getName()));
+        boolean isAdminSigned = request.isUserInRole("ROLE_ADMIN");
+        model.addAttribute("onlyCompleted", true);
+        model.addAttribute("to-dos", todoItemService.findAll());
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("signedUser", signedUser);
+        model.addAttribute("isAdminSigned", isAdminSigned);
+        return "redirect:/todos";
+    }
+
+    @GetMapping("/todos/overdue")
+    public String getToDoItemsOverdue(Model model, Principal principal, SecurityContextHolderAwareRequestWrapper request){
+
+        User signedUser = userService.getUserById(Long.parseLong(principal.getName()));
+        boolean isAdminSigned = request.isUserInRole("ROLE_ADMIN");
+        model.addAttribute("onlyOverdue", true);
+        model.addAttribute("to-dos", todoItemService.findAll());
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("signedUser", signedUser);
+        model.addAttribute("isAdminSigned", isAdminSigned);
+        return "redirect:/todos";
+    }
 
 
     @PostMapping("/todos/edit/{id}")
